aecd4f1b8acc9873bec4a1ec2f02a0c591efdf28
Myroslav <andreykiv09@gmail.com>
2021-02-02 16:24:26 +0100

    init commit

--- ComentarioVariasLineas.java
+++ ComentarioVariasLineas.java
@@ -1,10 +1,11 @@
 package testPack;
 
-public class ComentarioLinea {
+public class ComentarioVariasLineas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Adios");
+		for(int i=0;i<6;i++)
+			System.out.print(i + " ");
 		System.out.println("FIN DE PROGRAMA");
 	}
 
